ca493273f6b8101a6919bb82ab6cdb1f9fa464ab
BartPart <wazoowazol@hotmail.com>
2021-04-27 11:40:01 -0500

    Update config.js
    Test for Cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_JTi25cDVB',
-        userPoolClientId: '5qi4adsulhj6c35qgngict3car',
-        region: 'eu-west-1'
+        userPoolId: 'us-east-1_QWlZlMIOW',//e.g. 'eu-west-1_JTi25cDVB',
+        userPoolClientId: '2cm44a7jit1bvaeelo88apddlr',//e.g. '5qi4adsulhj6c35qgngict3car',
+        region: 'us-east-1' //'eu-west-1'
     },
     api: {
         invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
